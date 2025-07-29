864243b9f27d3abab6282cf4aae06688aa477dd1
danielendaylalu1 <endaylaludaniel@gmail.com>
2025-07-29 10:40:35 +0300

    fix: fixed build errors

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import handWave from " /gifs/handwave.gif";
+// import handWave from " /gifs/handwave.gif";
 
 const ComingSoon = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center gap-6">
-      <Image src={handWave} alt="hand wave" className="w-[62px]" />
+      <Image src="/gifs/handwave.gif" alt="hand wave" className="w-[62px]" />
       <h1 className="text-green-300 text-center text-xl">coming soon...</h1>
     </div>
   );
